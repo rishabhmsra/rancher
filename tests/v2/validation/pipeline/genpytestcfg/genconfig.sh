634011f5e323facf63b043ba0ea08c32afffd746
vardhaman22 <vardhaman.surana@suse.com>
2023-11-06 15:16:56 +0530

    fix pytestcfg script

--- tests/v2/validation/pipeline/genpytestcfg/genconfig.sh
+++ tests/v2/validation/pipeline/genpytestcfg/genconfig.sh
@@ -3,7 +3,7 @@ set -e
 cd $(dirname $0)/../../../../../
 
 echo "building release upgrade bin"
-env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o tests/v2/validation/pipeline/bin/genconfig ./tests/v2/validation/pipeline/genvalidationtestcfg
+env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o tests/v2/validation/pipeline/bin/genconfig ./tests/v2/validation/pipeline/genpytestcfg
 
 echo "running release upgrade"
 tests/v2/validation/pipeline/bin/genconfig
